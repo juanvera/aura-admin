b42b7297b653e4c77a61747394286ccef7984c3b
juanvera <juan.vera@edosoft.es>
2020-04-21 11:50:03 +0100

    Securing firebase.js config file in order to protect confidential data.

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   productionSourceMap: false,
   pwa: {
-    name: 'Aura Admin',
+    name: 'GDG Gran Canaria Admin Site',
     workboxPluginMode: 'InjectManifest',
     themeColor: '#4A90E2',
     msTileColor: '#4A90E2',
